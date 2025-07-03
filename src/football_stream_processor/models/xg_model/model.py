06e923766f56e9c7047c4528e0baecb94b3f5090
Smarth <bakshismarth.99@gmail.com>
2025-07-03 13:57:10 +0200

    Add xgboost model option

--- src/football_stream_processor/models/xg_model/model.py
+++ src/football_stream_processor/models/xg_model/model.py
@@ -1,6 +1,10 @@
 from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
 
-def get_linear_model():
-    # Logistic regression for binary classification (pass success/fail)
-    model = LogisticRegression(max_iter=1000)
-    return model
+def get_model(model_name: str = "logistic_regression"):
+    if model_name == "logistic_regression":
+      model = LogisticRegression(max_iter=1000)
+      return model
+    elif model_name == "xgboost":
+      model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+      return model
