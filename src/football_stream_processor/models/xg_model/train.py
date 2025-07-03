fdb36560fd8df685ed7f5f418658f7fba497d880
Smarth <bakshismarth.99@gmail.com>
2025-07-03 13:57:33 +0200

    Remove manual feature engineering

--- src/football_stream_processor/models/xg_model/train.py
+++ src/football_stream_processor/models/xg_model/train.py
@@ -1,20 +1,59 @@
 import os
-from sklearn.metrics import accuracy_score
-
-from .data_preparation import load_and_prepare_data
-from .model import get_linear_model
-from .evaluation import print_classification_report, print_roc_auc, plot_confusion_matrix
-from .utils import save_model
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from model import get_model
+from evaluation import print_classification_report, print_roc_auc, plot_confusion_matrix
+from utils import save_model
+from data_preparation import load_and_prepare_data
 
 def main():
-    # data_path = os.path.expanduser("~/data/pass_data.csv")  # Change path accordingly
-    model_save_path = os.path.expanduser("~/models/.pickle/linear_model.pkl")
     
+    model_save_path = os.path.expanduser("~/projects/football_stream_processor/models/linear_model.pkl")
+
     X_train, X_test, y_train, y_test = load_and_prepare_data()
-    
-    model = get_linear_model()
+
+    categorical_features = ["length_bucket", "minute_bucket"]
+    numerical_features = [
+        "start_x", "start_y", "end_x", "end_y",
+        "distance", "angle", "abs_angle"
+    ]
+    binary_features = [
+        "is_forward", "progressive",
+        "start_in_final_third", "end_in_penalty_area"
+    ]
+
+    categorical_transformer = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder(drop="first"))
+    ])
+
+    numerical_transformer = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler", StandardScaler())
+    ])
+
+    binary_transformer = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent"))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numerical_transformer, numerical_features),
+            ("cat", categorical_transformer, categorical_features),
+            ("bin", binary_transformer, binary_features)
+        ]
+    )
+
+    model = Pipeline(steps=[
+        ("preprocessor", preprocessor),
+        ("classifier", get_model("logistic_regression"))
+    ])
+
     model.fit(X_train, y_train)
-    
+    print("Model trained successfully.")
     y_pred = model.predict(X_test)
     y_probs = model.predict_proba(X_test)[:, 1]
     
