7b351f1f73ad9b8acf323cbad19639348d960d9e
Smarth Bakshi <bakshismarth.99@gmail.com>
2025-07-26 23:58:16 +0200

    Fix connection with MLFlow server

--- app/utils/mlflow_utils.py
+++ app/utils/mlflow_utils.py
@@ -1,14 +1,13 @@
 import mlflow
 from mlflow.tracking import MlflowClient
-from src.football_stream_processor.config import MLFLOW_EXPERIMENT_NAME
 
 def get_best_run():
     try:
         client = MlflowClient()
 
-        experiment = client.get_experiment_by_name(MLFLOW_EXPERIMENT_NAME)
+        experiment = client.get_experiment_by_name("football-pass-prediction")
         if experiment is None:
-            raise ValueError(f"No experiment found with name: {MLFLOW_EXPERIMENT_NAME}")
+            raise ValueError("No experiment found with name: football-pass-prediction")
 
         runs = client.search_runs(
             experiment_ids=[experiment.experiment_id],
