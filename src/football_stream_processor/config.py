55800c50747de7057fee5b1e48b7534edb5de011
Smarth Bakshi <bakshismarth.99@gmail.com>
2025-07-27 00:23:15 +0200

    Restructure directory

--- src/football_stream_processor/config.py
+++ src/football_stream_processor/config.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from pathlib import Path
 
 # Model training
 MODEL_NAME = "xgboost"
@@ -7,12 +8,16 @@ RANDOM_SEED = 42
 TEST_SIZE = 0.2
 
 # Paths
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 MODEL_DIR = "models"
 MODEL_FILENAME_TEMPLATE = "{model_name}_model.pkl"
 MODEL_SAVE_PATH = os.path.join(MODEL_DIR, MODEL_FILENAME_TEMPLATE.format(model_name=MODEL_NAME))
 PLOT_DIR = "resources/plots"
 PICKLE_DIR = ".pickle"
 RESOURCES_DIR = "resources"
+MLFLOW_DIR = ROOT_DIR / "mlflow"
+MLFLOW_RUNS = MLFLOW_DIR / "mlruns"
+MLFLOW_ARTIFACTS = MLFLOW_DIR / "mlartifacts"
 
 # MLflow Configuration
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://127.0.0.1:5000")  # Can also use local file path
