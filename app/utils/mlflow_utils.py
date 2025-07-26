1261e4dc7251bf193d08401a3a1d9243cb66ebb6
Smarth Bakshi <bakshismarth.99@gmail.com>
2025-07-27 01:10:58 +0200

    Use local mlflow runs

--- app/utils/mlflow_utils.py
+++ app/utils/mlflow_utils.py
@@ -1,13 +1,16 @@
 import mlflow
 from mlflow.tracking import MlflowClient
+from football_stream_processor.config import MLFLOW_TRACKING_URI, MLFLOW_EXPERIMENT_NAME
 
 def get_best_run():
     try:
-        client = MlflowClient()
+        
+        mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+        client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
 
-        experiment = client.get_experiment_by_name("football-pass-prediction")
+        experiment = client.get_experiment_by_name(MLFLOW_EXPERIMENT_NAME)
         if experiment is None:
-            raise ValueError("No experiment found with name: football-pass-prediction")
+            raise ValueError(f"No experiment found with name: {MLFLOW_EXPERIMENT_NAME}")
 
         runs = client.search_runs(
             experiment_ids=[experiment.experiment_id],
@@ -18,5 +21,5 @@ def get_best_run():
         return runs[0] if runs else None
 
     except Exception as e:
-        print(f"[MLFlow Error] {e}")
+        print(f"[MLflow Error] {e}")
         return None
